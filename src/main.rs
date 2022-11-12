951df2f76605240f4015700e1e13a16d0e0de4ed
zekroTJA <riho@live.de>
2022-11-12 10:55:41 +0000

    add better config support

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use clap::Parser;
+use config::{Config, ConfigError, File, FileFormat, Map, Source, Value, ValueKind};
+use directories::ProjectDirs;
+use minecraft_client_rs::Client;
 use std::{
     io::{self, Write},
+    path::Path,
     process::exit,
+    result,
 };
-
-use clap::Parser;
-
-use config::{Config, File, FileFormat};
-use minecraft_client_rs::Client;
 use string_join::Join;
 
-#[derive(Parser)]
+#[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Location of a server.proterties file to read credentials from.
@@ -28,42 +29,90 @@ struct Args {
     command: Vec<String>,
 }
 
+impl Source for Args {
+    fn clone_into_box(&self) -> Box<dyn Source + Send + Sync> {
+        Box::new(self.clone())
+    }
+
+    fn collect(&self) -> result::Result<Map<String, Value>, ConfigError> {
+        let mut m = Map::new();
+        let uri: String = "command line arguments".into();
+
+        if let Some(password) = &self.password {
+            m.insert(
+                "rcon.password".into(),
+                Value::new(Some(&uri), ValueKind::String(password.clone())),
+            );
+        }
+
+        if let Some(address) = &self.address {
+            let split: Vec<&str> = address.split(':').collect();
+
+            if let Some(ip) = split.get(0) {
+                m.insert(
+                    "server-ip".into(),
+                    Value::new(Some(&uri), ValueKind::String(ip.to_string())),
+                );
+            }
+
+            if let Some(port) = split.get(1) {
+                m.insert(
+                    "rcon.port".into(),
+                    Value::new(Some(&uri), ValueKind::String(port.to_string())),
+                );
+            }
+        }
+
+        Ok(m)
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
-    let mut addr = "localhost:25575".to_string();
-    let mut passwd: Option<String> = None;
+    // Try to load config from ./config.ini
+    let mut conf_builder =
+        Config::builder().add_source(File::from(Path::new("config.ini")).required(false));
 
-    if let Some(properties) = args.properties {
-        let conf = Config::builder()
-            .add_source(File::new(&properties, FileFormat::Ini).required(true))
-            .build()
-            .expect("Failed reading config from server properties");
-
-        let mut ip = conf.get_string("server-ip").unwrap_or_else(|_| "".into());
-        if ip.is_empty() {
-            ip = "localhost".into();
-        }
-
-        let mut port = conf.get_string("rcon.port").unwrap_or_else(|_| "".into());
-        if port.is_empty() {
-            port = "25575".into();
-        }
-
-        addr = format!("{ip}:{port}");
-
-        passwd = conf.get_string("rcon.password").ok();
+    // Try to load config from $CONFIG_DIR/config.ini
+    if let Some(dirs) = ProjectDirs::from("de", "zekro", "rconcli") {
+        let pth = dirs
+            .config_dir()
+            .to_str()
+            .expect("could not transform path to string");
+        conf_builder =
+            conf_builder.add_source(File::from(Path::new(pth).join("config.ini")).required(false));
     }
 
-    if let Some(arg_addr) = args.address {
-        addr = arg_addr;
+    // Load config from passed server.properties, if specified
+    if let Some(properties) = &args.properties {
+        conf_builder =
+            conf_builder.add_source(File::new(properties, FileFormat::Ini).required(true));
     }
 
-    if let Some(arg_passwd) = args.password {
-        passwd = Some(arg_passwd);
+    // Load config from command line parameters
+    conf_builder = conf_builder.add_source(args.clone());
+
+    let conf = conf_builder
+        .build()
+        .expect("Failed reading config from server properties");
+
+    let mut ip = conf.get_string("server-ip").unwrap_or_else(|_| "".into());
+    if ip.is_empty() {
+        ip = "localhost".into();
     }
 
-    let passwd = passwd.expect("No password has been provided");
+    let mut port = conf.get_string("rcon.port").unwrap_or_else(|_| "".into());
+    if port.is_empty() {
+        port = "25575".into();
+    }
+
+    let addr = format!("{ip}:{port}");
+    let passwd = conf
+        .get_string("rcon.password")
+        .expect("No password has been provided");
+
+    dbg!(&addr, &passwd);
 
     let mut client = Client::new(addr).expect("Failed connecting to server");
     client.authenticate(passwd).expect("Authentication failed");
